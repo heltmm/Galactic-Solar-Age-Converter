0886870779a4a88e0e28bac0004a7b3846e99fa5
Mark Helt <heltmm@gmail.com>
2017-10-13 10:41:24 -0700

    add converstion functions

--- js/solarcalc.js
+++ js/solarcalc.js
@@ -1,10 +1,26 @@
 export class Age {
-  constructor(species, birthDate){
+  constructor(species, birthday, age){
     this.species = species;
-    this.birthDate = birthDate;
+    this.birthday = birthday;
+    this.earth = age
   }
   toYears(){
-  
+
+  }
+  yearToSeconds(year){
+    return year * 31,536,000
+  }
+  mercury(){
+    return this.earth * .24
+  }
+  venus(){
+    return this.earth * .62
+  }
+  mars(){
+    return this.earth * 1.88
+  }
+  jupiter(){
+    return this.earth * 11.86
   }
 
 
