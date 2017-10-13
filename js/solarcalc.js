5d2f1de32474b05960f4960c510481c7c0e0e3c4
Mark Helt <heltmm@gmail.com>
2017-10-13 15:18:54 -0700

    determine life expectancy

--- js/solarcalc.js
+++ js/solarcalc.js
@@ -7,14 +7,15 @@ export class Age {
     this.seconds = 0;
     this.age = 0;
     this.life = 0;
+    this.lifeLeft = 0;
   }
 
   setLife(){
     if(this.species === "Human"){
-      if(birthday[0] > 1985){
+      if(this.birthday.getYear() > 85){
         this.life = 85;
       }
-      else if(bithday[0] > 1970){
+      else if(this.birthday.getYear() > 70){
         this.life = 83;
       }
       else{
@@ -24,35 +25,54 @@ export class Age {
     if(this.species === "Dog"){
       this.life = 11;
     }
-    return this.life
+    return this.life;
+  }
+  determineLife(){
+    return this.lifeLeft = this.life - this.age;
   }
 
   birthToSeconds(){
     let today_seconds = Date.parse(moment().format())/1000
     let birth_seconds = Date.parse(this.birthday)/1000
     this.seconds = today_seconds - birth_seconds;
-    return this.seconds
+    return this.seconds;
   }
 
   secondsToAge(){
     this.age = Math.floor(this.seconds / 31536000);
-    return this.age
+    return this.age;
   }
-
+  //convert years, life expentance, and years remaining on mercury
   mercury(){
-    return Math.floor(this.age * 0.24);
+    let mercury = [];
+    mercury.push(Math.floor(this.age * 0.24));
+    mercury.push(Math.floor(this.life *0.24));
+    mercury.push(mercury[1]-mercury[0])
+    return mercury;
   }
-
+  //convert years, life expentance, and years remaining on venus
   venus(){
-    return Math.floor(this.age * 0.62);
+    let venus = [];
+    venus.push(Math.floor(this.age * 0.62));
+    venus.push(Math.floor(this.life * 0.62));
+    venus.push(venus[1]-venus[0]);
+    return venus;
   }
-
+  //convert years, life expentance, and years remaining on mars
   mars(){
-    return Math.floor(this.age * 1.88);
+    let mars = []
+    mars.push(Math.floor(this.age * 1.88));
+    mars.push(Math.floor(this.life * 1.88));
+    mars.push(mars[1]-mars[0]);
+    return mars;
   }
-
+  //convert years, life expentance, and years remaining on jupiter
   jupiter(){
-    return Math.floor(this.age * 11.86);
+    let jupiter = [];
+    jupiter.push(Math.floor(this.age * 11.86));
+    jupiter.push(Math.floor(this.life * 11.86));
+    jupiter.push(jupiter[1] - jupiter[0]);
+    return jupiter;
   }
 }
 
--- spec/solarcalc-spec.js
+++ spec/solarcalc-spec.js
@@ -24,32 +24,108 @@ describe('Age', () => {
     expect(userAge.secondsToAge()).toEqual(30)
   });
 
+  it('calculate life expectancy on earth', () => {
+    let userAge = new Age("Human", [1987,7,10]);
+    userAge.birthToSeconds();
+    userAge.secondsToAge();
+    userAge.setLife();
+    expect(userAge.determineLife()).toEqual(55)
+  });
+
   it('calucate age on mercury', () => {
     let userAge = new Age("Human", [1987,7,10]);
     userAge.birthToSeconds();
     userAge.secondsToAge();
-    expect(userAge.mercury()).toEqual(7)
+    userAge.setLife();
+    expect(userAge.mercury()[0]).toEqual(7)
+  });
+
+  it('calucate life expetancy on Merury', () => {
+    let userAge = new Age("Human", [1987,7,10]);
+    userAge.birthToSeconds();
+    userAge.secondsToAge();
+    userAge.setLife();
+    expect(userAge.mercury()[1]).toEqual(20)
+  });
+
+  it('calucate years left on Merury', () => {
+    let userAge = new Age("Human", [1987,7,10]);
+    userAge.birthToSeconds();
+    userAge.secondsToAge();
+    userAge.setLife();
+    expect(userAge.mercury()[2]).toEqual(13)
   });
 
   it('calucate age on venus', () => {
     let userAge = new Age("Human", [1987,7,10]);
     userAge.birthToSeconds();
     userAge.secondsToAge();
-    expect(userAge.venus()).toEqual(18)
+    userAge.setLife();
+    expect(userAge.venus()[0]).toEqual(18)
+  });
+
+  it('calucate life expectancy on venus', () => {
+    let userAge = new Age("Human", [1987,7,10]);
+    userAge.birthToSeconds();
+    userAge.secondsToAge();
+    userAge.setLife();
+    expect(userAge.venus()[1]).toEqual(52)
+  });
+
+  it('calucate years left venus', () => {
+    let userAge = new Age("Human", [1987,7,10]);
+    userAge.birthToSeconds();
+    userAge.secondsToAge();
+    userAge.setLife();
+    expect(userAge.venus()[2]).toEqual(34)
   });
 
   it('calucate age on mars', () => {
     let userAge = new Age("Human", [1987,7,10]);
     userAge.birthToSeconds();
     userAge.secondsToAge();
-    expect(userAge.mars()).toEqual(56)
+    userAge.setLife();
+    expect(userAge.mars()[0]).toEqual(56)
+  });
+
+  it('calucate life expectancy on mars', () => {
+    let userAge = new Age("Human", [1987,7,10]);
+    userAge.birthToSeconds();
+    userAge.secondsToAge();
+    userAge.setLife();
+    expect(userAge.mars()[1]).toEqual(159)
+  });
+
+  it('calucate years left on mars', () => {
+    let userAge = new Age("Human", [1987,7,10]);
+    userAge.birthToSeconds();
+    userAge.secondsToAge();
+    userAge.setLife();
+    expect(userAge.mars()[2]).toEqual(103)
   });
 
   it('calucate age on jupiter', () => {
     let userAge = new Age("Human", [1987,7,10]);
     userAge.birthToSeconds();
     userAge.secondsToAge();
-    expect(userAge.jupiter()).toEqual(355)
+    userAge.setLife();
+    expect(userAge.jupiter()[0]).toEqual(355)
+  });
+
+  it('calucate life expectancy on jupiter', () => {
+    let userAge = new Age("Human", [1987,7,10]);
+    userAge.birthToSeconds();
+    userAge.secondsToAge();
+    userAge.setLife();
+    expect(userAge.jupiter()[1]).toEqual(1008)
+  });
+
+  it('calucate years left on jupiter', () => {
+    let userAge = new Age("Human", [1987,7,10]);
+    userAge.birthToSeconds();
+    userAge.secondsToAge();
+    userAge.setLife();
+    expect(userAge.jupiter()[2]).toEqual(653)
   });
 
 });
