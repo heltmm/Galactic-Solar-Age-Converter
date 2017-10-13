0f9443b7da7d2a94a340d989f07d2c5e98851d97
Mark Helt <heltmm@gmail.com>
2017-10-13 09:18:06 -0700

    create tests

---
+++ spec/solarcalc-spec.js
@@ -0,0 +1,28 @@
+import {Age} from './../js/solarcalc.js'
+
+describe('Age', () => {
+
+  it('create a new age', () => {
+    let userAge = new Age("Human", "08/07/1987");
+    expect(age.birthDate).toEqual("08/07/1987")
+  });
+
+  it('create a new age', () => {
+    let userAge = new Age("Human", "08/07/1987");
+    expect(age.species).toEqual("Human")
+  });
+
+  it('calucate age on earth', () => {
+    let userAge = new Age("Human", "08/07/1987");
+    expect(age.earthYears).toEqual(30)
+  });
+
+  it('calucate age on earth', () => {
+    let userAge = new Age("Human", "08/07/1987");
+    expect(age.earthYears).toEqual(30)
+  });
+
+  it('calucate earth years into seconds', () => {
+    let userAge = new Age("Human", "08/07/1987");
+    expect(age.earthYears.toSeconds).toEqual(946,080,000)
+  });
